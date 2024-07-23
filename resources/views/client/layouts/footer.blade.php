589997c3a6b917a35cdf09ca5342e4ccc7185dc3
NamJSdev <nam.js.dev@gmail.com>
2024-07-24 06:28:33 +0700

    update footer

--- resources/views/client/layouts/footer.blade.php
+++ resources/views/client/layouts/footer.blade.php
@@ -3,9 +3,8 @@
         <div class="row">
             <div class="col-lg-4">
                 <div class="widget">
-                    <h3 class="mb-4">About</h3>
-                    <p>Far far away, behind the word mountains, far from the countries Vokalia
-                        and Consonantia, there live the blind texts.</p>
+                    <h3 class="mb-4">Tổng Quan</h3>
+                    <p>DocSvnua Version 1.0.0 phiên bản thứ nghiệm của nhóm 3 môn "Xây Dựng Và Phát Triển Phần Mềm".</p>
                 </div> <!-- /.widget -->
                 <div class="widget">
                     <h3>Social</h3>
@@ -19,28 +18,6 @@
                     </ul>
                 </div> <!-- /.widget -->
             </div> <!-- /.col-lg-4 -->
-            <div class="col-lg-8 ps-lg-5">
-                <div class="widget" style="margin-left: 450px;">
-                    <h3 class="mb-4">Company</h3>
-                    <ul class="list-unstyled float-start links">
-                        <li><a href="#">About us</a></li>
-                        <li><a href="#">Services</a></li>
-                        <li><a href="#">Vision</a></li>
-                        <li><a href="#">Mission</a></li>
-                        <li><a href="#">Terms</a></li>
-                        <li><a href="#">Privacy</a></li>
-                    </ul>
-                    <ul class="list-unstyled float-start links">
-                        <li><a href="#">Partners</a></li>
-                        <li><a href="#">Business</a></li>
-                        <li><a href="#">Careers</a></li>
-                        <li><a href="#">Blog</a></li>
-                        <li><a href="#">FAQ</a></li>
-                        <li><a href="#">Creative</a></li>
-                    </ul>
-                </div> <!-- /.widget -->
-            </div> <!-- /.col-lg-4 -->
-
             <div class="row mt-5">
                 <div class="col-12 text-center">
                     <p>Copyright
